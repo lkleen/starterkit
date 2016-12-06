8356709f74addd9f10e9970b15d37cb5660032a8
Lars Kleen <github@larsworks.org>
2016-12-06 20:33:46 +0100

    language menu setup

--- site/snippets/menu.php
+++ site/snippets/menu.php
@@ -5,10 +5,12 @@
       <a href="<?= $item->url() ?>"><?= $item->title()->html() ?></a>
     </li>
     <?php endforeach ?>
+  </ul>
+  <ul class="navigation">
 	<?php foreach($site->languages() as $language): ?>
-    <li class="menu-item-language"<?= e($site->language() == $language, ' is-active') ?>>
+    <li class="menu-item<?= e($site->language() == $language, ' is-active')?>" >
       <a href="<?php echo $page->url($language->code()) ?>">
-        <?php echo html($language->name()) ?>
+        <?php echo html($language->code()) ?>
       </a>
     </li>
     <?php endforeach ?>
